4650eb863f0ce2ac5199e9fd042cacedb1680c1f
ASUS <krishapatel2399@gmail.com>
2025-11-16 15:15:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_24012012009_practical6/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.mad_24012012009_practical6
+
+import android.graphics.drawable.AnimationDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var alarmanimation : AnimationDrawable
+    lateinit var heartanimation : AnimationDrawable
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val alarm : ImageView = findViewById(R.id.alarmImage)
+        alarm.setBackgroundResource(R.drawable.alarm_animation_list)
+        alarmanimation = alarm.background as AnimationDrawable
+
+        val heart : ImageView = findViewById(R.id.heartIcon)
+        heart.setBackgroundResource(R.drawable.heart_animation_list)
+        heartanimation = heart.background as AnimationDrawable
+
+
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            alarmanimation.start()
+            heartanimation.start()
+
+
+        }
+        else {
+            alarmanimation.stop()
+            heartanimation.stop()
+        }
+
+
+    }
+}
